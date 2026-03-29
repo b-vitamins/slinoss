4707e2c2cb0e62b274ca693cde671369e38c6793
Ayan Das <bvits@riseup.net>
2026-03-29 13:05:31 +0530

    perf: tile chunk-increment boundary bwd

--- slinoss/ops/v2x2ssd/cute/kernels/bwd/chunk_increment/boundary.py
+++ slinoss/ops/v2x2ssd/cute/kernels/bwd/chunk_increment/boundary.py
@@ -105,6 +105,13 @@ class ChunkIncrementBwdBoundaryAmpere:
         warp = cute.arch.warp_idx()
 
         smem = cutlass.utils.SmemAllocator()
+        async_copy_bits = self.async_copy_bits
+        async_elems = async_copy_bits // mDInc.element_type.width
+        fast_tiled = (
+            self.P <= self.du_prev_threads
+            and self.D >= 128
+            and self.D % async_elems == 0
+        )
         s_Mp0 = smem.allocate_tensor(
             cutlass.Float32, cute.make_layout((2,), stride=(1,)), 8
         )
@@ -117,25 +124,35 @@ class ChunkIncrementBwdBoundaryAmpere:
         s_u_prev = smem.allocate_tensor(
             cutlass.Float32, cute.make_layout((self.P,), stride=(1,)), 4
         )
-        s_g = smem.allocate_tensor(
-            cutlass.Float32, cute.make_layout((self.D,), stride=(1,)), 4
-        )
-        s_b_decay = smem.allocate_tensor(
-            cutlass.Float32, cute.make_layout((self.D,), stride=(1,)), 4
-        )
-        s_dinc = smem.allocate_tensor(
-            mDInc.element_type,
-            cute.make_layout((self.P, self.D), stride=(self.D, 1)),
-            16,
-        )
-
         p = tidx
         while cute.elem_less(p, self.P):
             s_u_prev[p] = mUPrev[p, bhc].to(cutlass.Float32)
             p = p + self.num_threads
 
-        async_copy_bits = self.async_copy_bits
-        async_elems = async_copy_bits // mDInc.element_type.width
+        if cutlass.const_expr(fast_tiled):
+            d_stage = 128
+            d_smem_stride = d_stage + async_elems
+            s_b_decay = smem.allocate_tensor(
+                cutlass.Float32, cute.make_layout((d_stage,), stride=(1,)), 4
+            )
+            s_dinc = smem.allocate_tensor(
+                mDInc.element_type,
+                cute.make_layout((self.P, d_smem_stride), stride=(d_smem_stride, 1)),
+                16,
+            )
+        else:
+            d_stage = self.D
+            d_smem_stride = (
+                (self.D + async_elems - 1) // async_elems
+            ) * async_elems + async_elems
+            s_b_decay = smem.allocate_tensor(
+                cutlass.Float32, cute.make_layout((self.D,), stride=(1,)), 4
+            )
+            s_dinc = smem.allocate_tensor(
+                mDInc.element_type,
+                cute.make_layout((self.P, d_smem_stride), stride=(d_smem_stride, 1)),
+                16,
+            )
         d_vec = self.D // async_elems
         mDInc_vec = cute.make_tensor(
             mDInc.iterator,
@@ -147,7 +164,8 @@ class ChunkIncrementBwdBoundaryAmpere:
         s_dinc_vec = cute.make_tensor(
             s_dinc.iterator,
             cute.make_layout(
-                (self.P, d_vec, async_elems), stride=(self.D, async_elems, 1)
+                (self.P, d_smem_stride // async_elems, async_elems),
+                stride=(d_smem_stride, async_elems, 1),
             ),
         )
         atom_async_copy_dinc = cute.make_copy_atom(
@@ -157,22 +175,23 @@ class ChunkIncrementBwdBoundaryAmpere:
             mDInc.element_type,
             num_bits_per_copy=async_copy_bits,
         )
-        p = tidx
-        while cute.elem_less(p, self.P):
-            dv = 0
-            while cute.elem_less(dv, d_vec):
-                cute.copy(
-                    atom_async_copy_dinc,
-                    mDInc_vec[bhc, p, dv, None],
-                    s_dinc_vec[p, dv, None],
-                )
-                dv = dv + 1
-            d = d_vec * async_elems
-            while cute.elem_less(d, self.D):
-                s_dinc[p, d] = mDInc[bhc, p, d]
-                d = d + 1
-            p = p + self.num_threads
-        cute.arch.cp_async_commit_group()
+        if cutlass.const_expr(not fast_tiled):
+            p = tidx
+            while cute.elem_less(p, self.P):
+                dv = 0
+                while cute.elem_less(dv, d_vec):
+                    cute.copy(
+                        atom_async_copy_dinc,
+                        mDInc_vec[bhc, p, dv, None],
+                        s_dinc_vec[p, dv, None],
+                    )
+                    dv = dv + 1
+                d = d_vec * async_elems
+                while cute.elem_less(d, self.D):
+                    s_dinc[p, d] = mDInc[bhc, p, d]
+                    d = d + 1
+                p = p + self.num_threads
+            cute.arch.cp_async_commit_group()
 
         if tidx == 0:
             for w in cutlass.range(4, unroll=1):
@@ -246,72 +265,176 @@ class ChunkIncrementBwdBoundaryAmpere:
         mp0i = s_Mp0[1]
 
         nvec = self.D // 2
-        v = tidx
-        while cute.elem_less(v, nvec):
-            d0 = v * 2
-            bx = mBPrev[d0 + 0, bhc].to(cutlass.Float32)
-            by = mBPrev[d0 + 1, bhc].to(cutlass.Float32)
-            s_b_decay[d0 + 0] = mp0r * bx - mp0i * by
-            s_b_decay[d0 + 1] = mp0r * by + mp0i * bx
-            v = v + self.num_threads
+        if cutlass.const_expr(fast_tiled):
+            acc_du = cutlass.Float32(0.0)
+            p_row = tidx
+            db_m0 = cutlass.Float32(0.0)
+            db_m1 = cutlass.Float32(0.0)
+            d_stage_vec = d_stage // async_elems
+            n_d_tiles = (self.D + d_stage - 1) // d_stage
+            for d_tile in cutlass.range_constexpr(n_d_tiles):
+                d_base = cutlass.Int32(d_tile * d_stage)
+                d_tile_width = min(d_stage, self.D - d_tile * d_stage)
+                d_tile_vec = d_tile_width // async_elems
+                copy_task = tidx
+                total_copy_tasks = cutlass.Int32(self.P * d_tile_vec)
+                while cute.elem_less(copy_task, total_copy_tasks):
+                    p = copy_task // cutlass.Int32(d_tile_vec)
+                    dv = copy_task - p * cutlass.Int32(d_tile_vec)
+                    cute.copy(
+                        atom_async_copy_dinc,
+                        mDInc_vec[bhc, p, d_tile * d_stage_vec + dv, None],
+                        s_dinc_vec[p, dv, None],
+                    )
+                    copy_task = copy_task + self.num_threads
+                cute.arch.cp_async_commit_group()
 
-        cute.arch.cp_async_wait_group(0)
-        cute.arch.sync_threads()
+                v = tidx
+                while cute.elem_less(v, cutlass.Int32(d_tile_width // 2)):
+                    d0 = v * 2
+                    bx = mBPrev[d_base + d0 + 0, bhc].to(cutlass.Float32)
+                    by = mBPrev[d_base + d0 + 1, bhc].to(cutlass.Float32)
+                    s_b_decay[d0 + 0] = mp0r * bx - mp0i * by
+                    s_b_decay[d0 + 1] = mp0r * by + mp0i * bx
+                    v = v + self.num_threads
 
-        if cute.elem_less(tidx, cutlass.Int32(self.du_prev_threads)):
-            p = tidx
-            while cute.elem_less(p, self.P):
-                acc = cutlass.Float32(0.0)
-                d = 0
-                while cute.elem_less(d, self.D):
-                    acc = acc + s_dinc[p, d].to(cutlass.Float32) * s_b_decay[d]
-                    d = d + 1
-                mDUPrev[p, bhc] = acc.to(mDUPrev.element_type)
-                p = p + self.du_prev_threads
+                cute.arch.cp_async_wait_group(0)
+                cute.arch.sync_threads()
 
-        if not cute.elem_less(tidx, cutlass.Int32(self.du_prev_threads)):
-            d = tidx - self.du_prev_threads
-            d_stride = cutlass.Int32(self.db_prev_threads)
-            while cute.elem_less(d, self.D):
-                acc = cutlass.Float32(0.0)
-                for p in cutlass.range(self.P, unroll=1):
-                    acc = acc + s_u_prev[p] * s_dinc[p, d].to(cutlass.Float32)
-                s_g[d] = acc
-                d = d + d_stride
+                if cute.elem_less(
+                    tidx, cutlass.Int32(self.du_prev_threads)
+                ) and cute.elem_less(p_row, self.P):
+                    d = 0
+                    while cute.elem_less(d, cutlass.Int32(d_tile_width)):
+                        acc_du = (
+                            acc_du + s_dinc[p_row, d].to(cutlass.Float32) * s_b_decay[d]
+                        )
+                        d = d + 1
 
-        cute.arch.sync_threads()
+                if not cute.elem_less(tidx, cutlass.Int32(self.du_prev_threads)):
+                    db_local = tidx - self.du_prev_threads
+                    pair = db_local // 2
+                    half = db_local - pair * 2
+                    if cute.elem_less(pair, cutlass.Int32(d_tile_width // 2)):
+                        d0 = pair * 2
+                        g0 = cutlass.Float32(0.0)
+                        g1 = cutlass.Float32(0.0)
+                        p_base = half * 32
+                        for p_off in cutlass.range(32, unroll=1):
+                            p = cutlass.Int32(p_base + p_off)
+                            if cute.elem_less(p, self.P):
+                                up = s_u_prev[p]
+                                g0 = g0 + up * s_dinc[p, d0 + 0].to(cutlass.Float32)
+                                g1 = g1 + up * s_dinc[p, d0 + 1].to(cutlass.Float32)
+                        g0 = g0 + cute.arch.shuffle_sync_bfly(
+                            g0, offset=1, mask=-1, mask_and_clamp=31
+                        )
+                        g1 = g1 + cute.arch.shuffle_sync_bfly(
+                            g1, offset=1, mask=-1, mask_and_clamp=31
+                        )
+                        if half == cutlass.Int32(0):
+                            mDBPrev[d_base + d0 + 0, bhc] = (mp0r * g0 + mp0i * g1).to(
+                                mDBPrev.element_type
+                            )
+                            mDBPrev[d_base + d0 + 1, bhc] = (mp0r * g1 - mp0i * g0).to(
+                                mDBPrev.element_type
+                            )
 
-        v = tidx
-        while cute.elem_less(v, nvec):
-            d0 = v * 2
-            g0 = s_g[d0 + 0]
-            g1 = s_g[d0 + 1]
-            mDBPrev[d0 + 0, bhc] = (mp0r * g0 + mp0i * g1).to(mDBPrev.element_type)
-            mDBPrev[d0 + 1, bhc] = (mp0r * g1 - mp0i * g0).to(mDBPrev.element_type)
-            v = v + self.num_threads
+                            bx = mBPrev[d_base + d0 + 0, bhc].to(cutlass.Float32)
+                            by = mBPrev[d_base + d0 + 1, bhc].to(cutlass.Float32)
+                            db_m0 = db_m0 + (g0 * bx + g1 * by)
+                            db_m1 = db_m1 + (bx * g1 - by * g0)
 
-        if warp == cutlass.Int32(0):
-            v = lane
-            m0 = cutlass.Float32(0.0)
-            m1 = cutlass.Float32(0.0)
+                cute.arch.sync_threads()
+
+            if cute.elem_less(
+                tidx, cutlass.Int32(self.du_prev_threads)
+            ) and cute.elem_less(p_row, self.P):
+                mDUPrev[p_row, bhc] = acc_du.to(mDUPrev.element_type)
+
+            if not cute.elem_less(tidx, cutlass.Int32(self.du_prev_threads)):
+                for off in (16, 8, 4, 2, 1):
+                    db_m0 = db_m0 + cute.arch.shuffle_sync_bfly(
+                        db_m0, offset=off, mask=-1, mask_and_clamp=31
+                    )
+                    db_m1 = db_m1 + cute.arch.shuffle_sync_bfly(
+                        db_m1, offset=off, mask=-1, mask_and_clamp=31
+                    )
+                if lane == cutlass.Int32(0):
+                    warp_re_total[warp - cutlass.Int32(self.du_prev_threads // 32)] = (
+                        db_m0
+                    )
+                    warp_im_total[warp - cutlass.Int32(self.du_prev_threads // 32)] = (
+                        db_m1
+                    )
+        else:
+            v = tidx
             while cute.elem_less(v, nvec):
                 d0 = v * 2
                 bx = mBPrev[d0 + 0, bhc].to(cutlass.Float32)
                 by = mBPrev[d0 + 1, bhc].to(cutlass.Float32)
-                g0 = s_g[d0 + 0]
-                g1 = s_g[d0 + 1]
-                m0 = m0 + (g0 * bx + g1 * by)
-                m1 = m1 + (bx * g1 - by * g0)
-                v = v + 32
+                s_b_decay[d0 + 0] = mp0r * bx - mp0i * by
+                s_b_decay[d0 + 1] = mp0r * by + mp0i * bx
+                v = v + self.num_threads
 
-            for off in (16, 8, 4, 2, 1):
-                m0 = m0 + cute.arch.shuffle_sync_bfly(
-                    m0, offset=off, mask=-1, mask_and_clamp=31
-                )
-                m1 = m1 + cute.arch.shuffle_sync_bfly(
-                    m1, offset=off, mask=-1, mask_and_clamp=31
-                )
+            cute.arch.cp_async_wait_group(0)
+            cute.arch.sync_threads()
 
-            if lane == cutlass.Int32(0):
-                mDMp0[0, bhc] = m0
-                mDMp0[1, bhc] = m1
+            if cute.elem_less(tidx, cutlass.Int32(self.du_prev_threads)):
+                p = tidx
+                while cute.elem_less(p, self.P):
+                    acc = cutlass.Float32(0.0)
+                    d = 0
+                    while cute.elem_less(d, self.D):
+                        acc = acc + s_dinc[p, d].to(cutlass.Float32) * s_b_decay[d]
+                        d = d + 1
+                    mDUPrev[p, bhc] = acc.to(mDUPrev.element_type)
+                    p = p + self.du_prev_threads
+
+            if not cute.elem_less(tidx, cutlass.Int32(self.du_prev_threads)):
+                v = tidx - self.du_prev_threads
+                v_stride = cutlass.Int32(self.db_prev_threads)
+                m0 = cutlass.Float32(0.0)
+                m1 = cutlass.Float32(0.0)
+                while cute.elem_less(v, nvec):
+                    d0 = v * 2
+                    g0 = cutlass.Float32(0.0)
+                    g1 = cutlass.Float32(0.0)
+                    for p in cutlass.range(self.P, unroll=1):
+                        up = s_u_prev[p]
+                        g0 = g0 + up * s_dinc[p, d0 + 0].to(cutlass.Float32)
+                        g1 = g1 + up * s_dinc[p, d0 + 1].to(cutlass.Float32)
+                    mDBPrev[d0 + 0, bhc] = (mp0r * g0 + mp0i * g1).to(
+                        mDBPrev.element_type
+                    )
+                    mDBPrev[d0 + 1, bhc] = (mp0r * g1 - mp0i * g0).to(
+                        mDBPrev.element_type
+                    )
+
+                    bx = mBPrev[d0 + 0, bhc].to(cutlass.Float32)
+                    by = mBPrev[d0 + 1, bhc].to(cutlass.Float32)
+                    m0 = m0 + (g0 * bx + g1 * by)
+                    m1 = m1 + (bx * g1 - by * g0)
+                    v = v + v_stride
+
+                for off in (16, 8, 4, 2, 1):
+                    m0 = m0 + cute.arch.shuffle_sync_bfly(
+                        m0, offset=off, mask=-1, mask_and_clamp=31
+                    )
+                    m1 = m1 + cute.arch.shuffle_sync_bfly(
+                        m1, offset=off, mask=-1, mask_and_clamp=31
+                    )
+                if lane == cutlass.Int32(0):
+                    warp_re_total[warp - cutlass.Int32(self.du_prev_threads // 32)] = m0
+                    warp_im_total[warp - cutlass.Int32(self.du_prev_threads // 32)] = m1
+
+        cute.arch.sync_threads()
+
+        if tidx == 0:
+            m0 = cutlass.Float32(0.0)
+            m1 = cutlass.Float32(0.0)
+            for w in cutlass.range(self.db_prev_threads // 32, unroll=1):
+                m0 = m0 + warp_re_total[w]
+                m1 = m1 + warp_im_total[w]
+            mDMp0[0, bhc] = m0
+            mDMp0[1, bhc] = m1
